0334c147151a5e314bad8cd68fecb73a454343b7
Michael Stone <vector_flys@hotmail.com>
2024-05-06 16:26:57 -0600

    add circle outline

--- src/shapes/draw.ts
+++ src/shapes/draw.ts
@@ -1,13 +1,11 @@
 import { CanvasRenderingContext2D } from "canvas";
 
-export function fill(ctx: CanvasRenderingContext2D, color: string)
-{
-    ctx.fillStyle = color;
-    ctx.fill();
+export function fill(ctx: CanvasRenderingContext2D, color: string) {
+  ctx.fillStyle = color;
+  ctx.fill();
 }
 
-export function stroke(ctx: CanvasRenderingContext2D, color: string)
-{
-    ctx.strokeStyle = color;
-    ctx.stroke();
+export function stroke(ctx: CanvasRenderingContext2D, color: string) {
+  ctx.strokeStyle = color;
+  ctx.stroke();
 }
